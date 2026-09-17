0fbd3b251576e4009f01b6c42863e332ccfaebe1
Jayesh Marathe <jayesh.marathe@congninovaindia.com>
2026-09-17 15:58:25 +0530

    learn funcitons in typescript

---
+++ typescript/04-functions.ts
@@ -0,0 +1,12 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(add(10, 20));
+
+
+function greet(name: string) : string{
+    return "jayesh and "+ name + " learn typescript";
+}
+
+console.log(greet("milind"));
