1bed0b9ae19449cfae09b428fe704516bde0d818
Jayesh Marathe <jayesh.marathe@congninovaindia.com>
2026-09-17 10:54:46 +0530

    leran variables

---
+++ 01-basic/variables.js
@@ -0,0 +1,46 @@
+// ==========================================
+// JavaScript Variables
+// ==========================================
+
+// 1. Using let
+// Use let when the value can change
+
+let age = 20;
+
+console.log("Initial age:", age);
+
+age = 21;
+
+console.log("Updated age:", age);
+
+
+// 2. Using const
+// Use const when the value should not be reassigned
+
+const country = "India";
+
+console.log("Country:", country);
+
+
+// 3. Using var
+// Older way of declaring variables
+
+var city = "Jalgaon";
+
+console.log("City:", city);
+
+
+// 4. Multiple variables
+
+const name = "Jayesh";
+let experience = 0;
+
+console.log("Name:", name);
+console.log("Experience:", experience);
+
+
+// 5. Updating a let variable
+
+experience = 1;
+
+console.log("Updated experience:", experience);
