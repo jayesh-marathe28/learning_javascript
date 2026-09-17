04da7755e8670f793f20162b73ddd2be1fa3f62e
Jayesh Marathe <jayesh.marathe@congninovaindia.com>
2026-09-17 10:51:16 +0530

    Learn datatypes

---
+++ 01-basic/datatypes.js
@@ -0,0 +1,73 @@
+// ==========================================
+// JavaScript Data Types
+// ==========================================
+
+// 1. String
+let name = "Jayesh";
+
+console.log("Name:", name);
+console.log("Type:",  typeof name);
+
+
+
+// 2. Number
+let age = 22;
+let price = 99.50;
+
+console.log("Age:", age);
+console.log("Type:", typeof age);
+
+console.log("Price:", price);
+console.log("Type:", typeof price);
+
+
+// 3. Boolean
+let isStudent = true;+
+
+console.log("Is Student:", isStudent);
+console.log("Type:", typeof isStudent);
+
+
+// 4. Undefined
+let city;
+
+console.log("City:", city);
+console.log("Type:", typeof city);
+
+
+// 5. Null
+let result = null;
+
+console.log("Result:", result);
+console.log("Type:", typeof result);
+
+
+// 6. BigInt
+let bigNumber = 12345678901234567890n;
+
+console.log("Big Number:", bigNumber);
+console.log("Type:", typeof bigNumber);
+
+
+// 7. Symbol
+let id = Symbol("id");
+
+console.log("Symbol:", id);
+console.log("Type:", typeof id);
+
+
+// 8. Object
+let student = {
+    name: "Jayesh",
+    age: 22
+};
+
+console.log("Student:", student);
+console.log("Type:", typeof student);
+
+
+// 9. Array
+let numbers = [10, 20, 30];
+
+console.log("Numbers:", numbers);
+console.log("Type:", typeof numbers);
