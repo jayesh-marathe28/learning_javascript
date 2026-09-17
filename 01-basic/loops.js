2e77fb5e4f49957ebadec4ea60f72a6be96fabef
Jayesh Marathe <jayesh.marathe@congninovaindia.com>
2026-09-17 10:52:52 +0530

    understand loops concept

---
+++ 01-basic/loops.js
@@ -0,0 +1,45 @@
+// ==========================================
+// JavaScript Loops
+// ==========================================
+
+
+// 1. for loop
+
+console.log("For Loop:");
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+
+// 2. while loop
+
+console.log("While Loop:");
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+
+// 3. do...while loop
+
+console.log("Do While Loop:");
+
+let number = 1;
+
+do {
+    console.log(number);
+    number++;
+} while (number <= 5);
+
+
+// 4. Print numbers 1 to 10
+
+console.log("Numbers 1 to 10:");
+
+for (let j = 1; j <= 10; j++) {
+    console.log(j);
+}
