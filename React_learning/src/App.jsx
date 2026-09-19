a020f57afbf586eb739394c0edefaa4e01300e14
Jayesh Marathe <jayesh.marathe@congninovaindia.com>
2026-09-19 17:08:17 +0530

    Add React learning app

---
+++ React_learning/src/App.jsx
@@ -0,0 +1,39 @@
+import JSX from "./01-JSX";
+import Components from "./02-Components";
+import Props from "./03-Props";
+import State from "./04-State";
+
+function App() {
+  return (
+    <div>
+      <h1>React Learning</h1>
+
+      <JSX />
+
+      <hr />
+
+      <Components />
+
+      <hr />
+
+      <Props />
+
+      <hr />
+
+      <State />
+
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+// import JSX from "./01-JSX";
+
+// function App() {
+//   return <JSX />;
+// }
+
+// export default App;
