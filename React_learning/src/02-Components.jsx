39775613c5ecb3cabc5dbe71e8d1c5978e228bde
Jayesh Marathe <jayesh.marathe@congninovaindia.com>
2026-09-19 17:07:16 +0530

    Learn  React components example

---
+++ React_learning/src/02-Components.jsx
@@ -0,0 +1,20 @@
+
+function Header() {
+  return <h1>My Website</h1>;
+}
+
+function Footer() {
+  return <p>Copyright 2026</p>;
+}
+
+
+function Components() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Components;
