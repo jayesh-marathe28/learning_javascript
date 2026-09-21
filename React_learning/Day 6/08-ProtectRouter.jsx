7f50776286c20c5017eb1911f17b2a3c9eb3f616
Jayesh Marathe <jayesh.marathe@congninovaindia.com>
2026-09-21 17:56:16 +0530

    Learn Protected Route

---
+++ React_learning/Day 6/08-ProtectRouter.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+    const isLoggedIn = true;
+
+    if (!isLoggedIn) {
+        return <Navigate to="/login" />;
+    }
+    return children;
+}
